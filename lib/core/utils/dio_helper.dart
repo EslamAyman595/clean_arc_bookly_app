839a2bac1bb880b165838c2edaae0893419189fc
Eslam Ayman <eslamaimn595@gmail.com>
2024-12-24 16:31:36 +0200

    create dio_helper

---
+++ lib/core/utils/dio_helper.dart
@@ -0,0 +1,23 @@
+import 'package:clean_arch_bookly_pp/core/utils/end_point.dart';
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  static late Dio dio;
+  static int() {
+    dio = Dio(BaseOptions(baseUrl: baseUrl, receiveDataWhenStatusError: true));
+  }
+
+  Future<Response> getData(
+      {required String url,
+       Map<String, dynamic>? query,
+       }) async {
+    return await dio.get(url, queryParameters: query);
+  }
+   Future<Response> postData(
+      {required String url, 
+      Map<String, dynamic>? query,
+        Map<String, dynamic>? data,
+      }) async {
+    return await dio.post(url, queryParameters: query,data: data);
+  }
+}
