d63c71d518afaf3c1dfb68e3aadf4715876ba2da
Eslam Ayman <eslamaimn595@gmail.com>
2024-12-24 16:41:34 +0200

    create hom_remote_data_source

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -13,7 +13,7 @@ class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, int> {
   @override
   Future<Either<Failure, List<BookEntity>>> call([int param = 0]) async {
     return await homeRepo.fetchFeaturedBooks(
-      pageNumber: param,
+      //pageNumber: param,
     );
   }
 }
