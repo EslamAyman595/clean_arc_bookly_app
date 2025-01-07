49b623515c8ee615af1d965a5f0ff58c685b6b4f
Eslam Ayman <eslamaimn595@gmail.com>
2025-01-07 13:00:26 +0200

    implement cubit newest books

--- lib/Features/home/presentation/manager/newset_book_cubit/cubit/newset_book_cubit.dart
+++ lib/Features/home/presentation/manager/newset_book_cubit/cubit/newset_book_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_arch_bookly_pp/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 import '../../../../domain/entities/book_entity.dart';
@@ -6,5 +7,15 @@ import '../../../../domain/entities/book_entity.dart';
 part 'newset_book_state.dart';
 
 class NewsetBookCubit extends Cubit<NewsetBookState> {
-  NewsetBookCubit() : super(NewsetBookInitial());
+  NewsetBookCubit(this.fetchNewestdBooksUseCase) : super(NewsetBookInitial());
+  final FetchNewestdBooksUseCase fetchNewestdBooksUseCase;
+  Future <void>fetchNewestBooks()async{
+    emit(NewsetBookLoading());
+    var result= await fetchNewestdBooksUseCase.call();
+    result.fold((failure) {
+      emit(NewsetBookFailure(errMassage: failure.message));
+    }, (books) {
+      emit(NewsetBookSucess(books: books));
+    });
+  }
 }
